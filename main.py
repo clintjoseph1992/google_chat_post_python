195c61ba734f07a36d26fd743b1490db296b7221
Clint Joseph <clint.jo@endurance.com>
2020-01-21 18:49:33 +0530

    Updated the chat bot script

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from httplib2 import Http
 from json import dumps
 import ast
 
-retry_timeout=7
-retry_max_count=3
+retry_timeout=7 #Configurable
+retry_max_count=3 #Configurable
 notification_url=<provide the webhook url here>
 
 def post_to_data_google_chat(gcmsg):
@@ -20,10 +20,12 @@ def post_to_data_google_chat(gcmsg):
                headers=message_headers,
                body=dumps(gcmsg),
            )
-           return ast.literal_eval(response[1])['thread']
+           decoded_ouput=response[1].decode("utf-8")
+           json_resp=json.loads(decoded_ouput)
+           return json_resp['thread']
 
-       except Exception,e:
-           print ('Not able to post message to google chat API')
+       except Exception as e:
+           print ('Not able to post message to google chat API'+str(e))
            print ("Retrying after " + str(retry_timeout) + " Seconds")
            time.sleep(retry_timeout)
            if i < retry_max_count-1:
@@ -34,14 +36,14 @@ def post_to_data_google_chat(gcmsg):
                sys.exit(1)
        break
 
-
-
 def google_chat_notify(msg):
+    """Convert the entire message into chunks if the lenghth is more than 4096 bytes"""
+    # print ("Length of msg: "+str(len(msg)))
     if len(msg) < 4096:
-        get_my_chat_thread=post_to_data_google_chat(gcmsg={'text':'@all','thread':{'name':''}})
+        get_my_chat_thread=post_to_data_google_chat(gcmsg={'text':'<users/all>','thread':{'name':''}})
         post_to_data_google_chat(gcmsg={'text':msg,'thread':get_my_chat_thread})
     else:
-        get_my_chat_thread=post_to_data_google_chat(gcmsg={'text':'@all','thread':{'name':''}})
+        get_my_chat_thread=post_to_data_google_chat(gcmsg={'text':'<users/all>','thread':{'name':''}})
         total_size=len(msg)
         max_chunks=total_size/4096
         start_pos=0
@@ -57,3 +59,9 @@ def google_chat_notify(msg):
                 end_pos=end_pos+4096
             post_to_data_google_chat(gcmsg={'text':msg_chunk,'thread':get_my_chat_thread})
 
+def main():
+    msg="Hello World"
+    google_chat_notify(msg)
+
+if __name__=="__main__":
+   main()
